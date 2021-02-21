e5b75299835428d6959557f485cd4ac69359645e
LUdanova <irynaludanova@gmail.com>
2021-02-21 17:47:31 +0200

    Creating methods for working with elements

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 import $ from "./lib/lib";
 
 $("button").on("click", function () {
-  $(this).toggleClass("active");
+  $("div").eq(2).toggleClass("active");
 });
+
+$("div").click(function () {
+  // console.log($(this).index());
+});
+//console.log($("div").eq(2).find(".some"));
+//console.log($(".some").closest(".findme"));
+console.log($(".more").eq(0).siblings());
